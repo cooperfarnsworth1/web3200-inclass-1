0fa38bae4331a6b4662f322c8670ad9535c3acd0
cooperfarnsworth1 <cooperfarnsworth1@mail.weber.edu>
2023-11-06 19:30:43 -0700

    appointment edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 ActiveRecord::Base.transaction do
   u = User.create(email: "admin@tutorme.com", password: "test1234", admin: true)
 
   Course.create!(prefix: 'WEB', num: 3200, title: 'Dynamic languages for WEB', 
-    description: "TBD", created_by: u, updated_by: u
-  )
+    description: "TBD", created_by: u, updated_by: u)
+  
+  User.create(email: 'test@tutorme.com', password: 'test1234', admin: false)
+  Tutor.create(name: 'John Smith', email: 'test@tutorme.com', bio: 'test')
 end
