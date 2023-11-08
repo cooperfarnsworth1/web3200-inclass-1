5b6b879e045ef22c0c000e92c9dbe5c567e40e17
Hunter Ferrin <hunterferrin@mail.weber.edu>
2023-11-07 21:45:18 -0700

    Styled Tutors pages(form, new, etc)

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,6 @@
 
 ActiveRecord::Base.transaction do
   u = User.create(email: "admin@tutorme.com", password: "test1234", admin: true)
-
   Course.create!(prefix: 'WEB', num: 3200, title: 'Dynamic languages for WEB', 
     description: "TBD", created_by: u, updated_by: u
   )
